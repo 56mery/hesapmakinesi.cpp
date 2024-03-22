2969da944ffd0feb2830aea1df3f8068f90d9da0
56mery <143891955+56mery@users.noreply.github.com>
2024-03-22 23:47:04 +0300

    New hesapmakinesi.cpp

--- hesapmakinesi.cpp
+++ hesapmakinesi.cpp
@@ -1,102 +1,17 @@
 //hesap makinesi etap1
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
+    char dizi1[10], dizi2[10];
     char islem;
-    long double sonuc = 0, x, y, d = 1, z, isinizbittimi = 1;
+    long double sonuc = 0, x, y, z;
     cout << "\t\t\t\t\t~~Hesap Makinesi~~" << endl;
 
-    while (isinizbittimi)
-    {
-        cout << "İlk sayiyi giriniz: ";
-        cin >> x;
-        cout << "Diger sayiyi giriniz: ";
-        cin >> y;
-        cout << "Yapmak istediginiz islemi giriniz: ";
-        cin >> islem;
-
-        if (islem == '+')
-        {
-            sonuc = (x + y);
-            cout << "sonuc: " << sonuc << endl;
-        }
-        if (islem == '-')
-        {
-            sonuc = (x - y);
-            cout << "sonuc: " << sonuc << endl;
-        }
-        if (islem == '*')
-        {
-            sonuc = (x * y);
-            cout << "sonuc: " << sonuc << endl;
-        }
-        if (islem == '/')
-        {
-            sonuc = (x / y);
-            cout << "sonuc: " << sonuc << endl;
-        }
-
-        d = 1;
-        while (d)
-        {
-            cout << sonuc << " sonucundan devam etmek ister misiniz, yoksa farklı bir yol mu izleyeceksin? evet ise 1 hayir ise 0'a basiniz: ";
-            cin >> d;
-
-            if (d == 0)
-                break;
-
-            cout << "Diğer sayiyi giriniz: ";
-            cin >> z;
-            cout << "Yapmak istediginiz islemi giriniz: ";
-            cin >> islem;
-
-            if (islem == '+')
-            {
-                sonuc = (sonuc + z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '-')
-            {
-                sonuc = (sonuc - z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '*')
-            {
-                sonuc = (sonuc * z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '/')
-            {
-                sonuc = (sonuc / z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-        }
-
-        cout << "Hesap makinesini tekrar kullanmak ister misiniz? ever ise bir hayir ise sifira basiniz: ";
-        cin >> isinizbittimi;
-        if (isinizbittimi == 0)
-            break;
-    }
-
-    return 0;
-}
-
-
-
-//hesap makinesi first fix try
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    char islem;
-    long double sonuc = 0, x, y, d = 1, z, isinizbittimi = 1;
-    cout << "\t\t\t\t\t~~Hesap Makinesi~~" << endl;
-
-    while (isinizbittimi)
+    while (1)
     {
         cout << "İlk sayiyi giriniz: ";
         cin >> x;
@@ -113,7 +28,7 @@ int main()
             }
             else
             {
-                cout << "olmayan islem girdiniz. tekrar deneyiniz:  ";
+                cout << "Olmayan islem girdiniz. Tekrar deneyiniz:  ";
                 cin >> islem;
             }
         }
@@ -139,67 +54,96 @@ int main()
             cout << "sonuc: " << sonuc << endl;
         }
 
-        d = 1;
-        while (d)
+        while (1)
         {
-            cout << sonuc << " sonucundan devam etmek ister misiniz, yoksa farklı bir yol mu izleyeceksin? evet ise 1 hayir ise 0'a basiniz: ";
-            cin >> d;
+            cout << sonuc << " sonucundan devam etmek ister misiniz? " << endl;
+            cin >> dizi1;
 
-            if (d == 0)
+            if (strcmp(dizi1, "hayir") == 0)
                 break;
 
-            cout << "Diğer sayiyi giriniz: ";
-            cin >> z;
-            cout << "Yapmak istediginiz islemi giriniz: ";
-            cin >> islem;
+            else if (strcmp(dizi1, "evet") == 0)
+            {
 
-            while (1)
-            {
-                if (islem == '+' || islem == '-' || islem == '*' || islem == '/')
+                cout << "Diğer sayiyi giriniz: ";
+                cin >> z;
+                cout << "Yapmak istediginiz islemi giriniz: ";
+                cin >> islem;
+
+                while (1)
                 {
-                    break;
+                    if (islem == '+' || islem == '-' || islem == '*' || islem == '/')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        cout << "Olmayan islem girdiniz. Tekrar deneyiniz:  ";
+                        cin >> islem;
+                    }
                 }
-                else
+
+                if (islem == '+')
                 {
-                    cout << "olmayan islem girdiniz. tekrar deneyiniz:  ";
-                    cin >> islem;
+                    sonuc = (sonuc + z);
+                    cout << "sonuc: " << sonuc << endl;
+                    continue;
+                }
+                if (islem == '-')
+                {
+                    sonuc = (sonuc - z);
+                    cout << "sonuc: " << sonuc << endl;
+                    continue;
+                }
+                if (islem == '*')
+                {
+                    sonuc = (sonuc * z);
+                    cout << "sonuc: " << sonuc << endl;
+                    continue;
+                }
+                if (islem == '/')
+                {
+                    sonuc = (sonuc / z);
+                    cout << "sonuc: " << sonuc << endl;
+                    continue;
                 }
             }
 
-            if (islem == '+')
+            else
             {
-                sonuc = (sonuc + z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '-')
-            {
-                sonuc = (sonuc - z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '*')
-            {
-                sonuc = (sonuc * z);
-                cout << "sonuc: " << sonuc << endl;
-            }
-            if (islem == '/')
-            {
-                sonuc = (sonuc / z);
-                cout << "sonuc: " << sonuc << endl;
+                cout << "Gecersiz cevap girdiniz. Evet ve ya hayir giriniz" << endl;
+                continue;
             }
         }
 
-        cout << "Hesap makinesini tekrar kullanmak ister misiniz? ever ise bir hayir ise sifira basiniz: ";
-        cin >> isinizbittimi;
-        if (isinizbittimi == 0)
+        cout << "Hesap makinesini tekrar kullanmak ister misiniz? ";
+        cin >> dizi2;
+
+        if (strcmp(dizi2, "hayir") == 0)
             break;
+
+        else if (strcmp(dizi2, "evet") == 0)
+            continue;
+
+        else
+        {
+            while (1)
+            {
+                cout << "Gecersiz cevap girdiniz. Evet ya da hayir'i deneyiniz: ";
+                cin >> dizi2;
+                if (strcmp(dizi2, "hayir") == 0)
+                    break;
+                else if (strcmp(dizi2, "evet") == 0)
+                    break;
+            }
+
+            if (strcmp(dizi2, "evet") == 0)
+                continue;
+
+            else if (strcmp(dizi2, "hayir") == 0)
+                break;
+        }
     }
 
     return 0;
 }
-
-//tek hatasi islem kismina q ya da w gibi tek bir şey girersen engelliyor. eğer qwe yazarsan uc kere cout veriyor yine veri almayi engelliyot ama
-//cout veriyor cok fazla
-
-
-//evet hayiri da dizilerle evet ve hayir aldim ama olmadi pointers denedim yine yapamadim. ekstra ond da evet ya da hayir 
-//dısında bir sey yazarsa tekrar alsın diye uğraştim olmadı. tekrar denicem.
